813a7ceb00170a4685c17231571a20274de5cc04
Admin <ductmph12511@fpt.edu.vn>
2021-05-28 12:18:29 +0700

    update firebase path

--- app/src/main/java/com/example/appgym/admin/AddActivity.java
+++ app/src/main/java/com/example/appgym/admin/AddActivity.java
@@ -8,9 +8,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.Activity;
 import android.content.ContentResolver;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.ImageDecoder;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.webkit.MimeTypeMap;
@@ -18,15 +23,15 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.MediaController;
 import android.widget.ProgressBar;
 import android.widget.Spinner;
-import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.VideoView;
 
 import com.example.appgym.R;
-import com.example.appgym.Video;
+import com.example.appgym.model.Video;
 import com.google.android.gms.tasks.Continuation;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -40,24 +45,28 @@ import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 public class AddActivity extends AppCompatActivity {
     static final int PICK_VIDEO = 1;
+    static final int PICK_IMAGE = 2;
     VideoView videoView;
     Button btnUpload;
     ProgressBar progressBar;
-    EditText edtName;
-    Uri videoUri;
+    EditText edtName,edtCalo;
+    Uri videoUri,imageUri;
     MediaController mediaController;
-    StorageReference storageReference;
+    StorageReference storageReferenceVideo,storageReferenceImage;
     DatabaseReference databaseReference;
     Video video;
-    UploadTask uploadTask;
+    UploadTask uploadTaskVideo,uploadTaskImage;
     Button btnChoose;
     ActionBar toolbar;
     Spinner spinnerChallenge;
+    ImageView imageView;
     List<String> list;
+    Bitmap bitmap;
     String group;
 
     @Override
@@ -65,13 +74,16 @@ public class AddActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
         video = new Video();
-        storageReference = FirebaseStorage.getInstance().getReference("Video");
+        storageReferenceVideo = FirebaseStorage.getInstance().getReference("Videos");
+        storageReferenceImage = FirebaseStorage.getInstance().getReference("Images");
         btnUpload = findViewById(R.id.button_upload_main);
         progressBar = findViewById(R.id.progressBar_main);
-        edtName = findViewById(R.id.et_video_name);
+        edtName = findViewById(R.id.edt_video_name);
+        edtCalo = findViewById(R.id.edt_calo);
         btnChoose = findViewById(R.id.btn_chooseVideo);
         mediaController = new MediaController(this);
         spinnerChallenge = findViewById(R.id.spinner_challenge);
+        imageView = findViewById(R.id.imgview_main);
         group = "Chest";
         videoView = findViewById(R.id.videoview_main);
         videoView.start();
@@ -122,7 +134,7 @@ public class AddActivity extends AppCompatActivity {
     @Override
     public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode == PICK_VIDEO || resultCode == Activity.RESULT_OK)
+        if(requestCode == PICK_VIDEO)
         {
             try
             {
@@ -146,7 +158,30 @@ public class AddActivity extends AppCompatActivity {
             }
 
         }
+        if (requestCode == PICK_IMAGE || requestCode == Activity.RESULT_OK)
+        {
+            try {
+                if(data != null)
+                {
+                    imageUri = data.getData();
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                        bitmap = ImageDecoder.decodeBitmap(ImageDecoder.createSource(getContentResolver(), imageUri));
+                    }else {
+                        bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), imageUri);
+                    }
+                    imageView.setImageBitmap(bitmap);
 
+                }
+            }catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+        }
+        if(videoUri != null)
+        {
+            videoView.setVideoURI(videoUri);
+            videoView.start();
+        }
     }
 
     public void ChooseVideo(View view)
@@ -155,6 +190,13 @@ public class AddActivity extends AppCompatActivity {
         intent.setType("video/*");
         intent.setAction(Intent.ACTION_GET_CONTENT);
         startActivityForResult(intent, PICK_VIDEO);
+    }
+    public void ChooseImage(View view)
+    {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, PICK_IMAGE);
     }
 
     private String getExt(Uri uri)
@@ -167,8 +209,9 @@ public class AddActivity extends AppCompatActivity {
     private void UploadVideo()
     {
         String videoName = edtName.getText().toString();
+        String calo = edtCalo.getText().toString();
         String search = edtName.getText().toString().toLowerCase();
-        if(videoUri != null || !TextUtils.isEmpty(videoName))
+        if(videoUri != null && imageUri != null && !TextUtils.isEmpty(videoName) && !TextUtils.isEmpty(calo))
         {
             ChangePathFireBase();
             databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
@@ -181,17 +224,20 @@ public class AddActivity extends AppCompatActivity {
                     else
                     {
                         progressBar.setVisibility(View.VISIBLE);
-                        final StorageReference reference = storageReference.child(System.currentTimeMillis() + "." + getExt(videoUri));
-                        uploadTask = reference.putFile(videoUri);
+                        final StorageReference referenceVideo = storageReferenceVideo.child(videoName + "." + getExt(videoUri));
+                        final StorageReference referenceImage = storageReferenceImage.child(videoName + "." + getExt(imageUri));
+                        uploadTaskVideo = referenceVideo.putFile(videoUri);
+                        uploadTaskImage = referenceImage.putFile(imageUri);
 
-                        Task<Uri> uriTask = uploadTask.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
+                        Task<Uri> uriTask = uploadTaskVideo.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
                             @Override
                             public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
                                 if(!task.isSuccessful())
                                 {
                                     throw task.getException();
                                 }
-                                return reference.getDownloadUrl();
+
+                                return referenceVideo.getDownloadUrl();
                             }
                         })
                                 .addOnCompleteListener(new OnCompleteListener<Uri>() {
@@ -200,14 +246,36 @@ public class AddActivity extends AppCompatActivity {
                                         if(task.isSuccessful())
                                         {
                                             Uri downloadUrl = task.getResult();
-                                            progressBar.setVisibility(View.INVISIBLE);
-                                            Toast.makeText(AddActivity.this,"Data saved", Toast.LENGTH_SHORT).show();
+                                            video.setVideoUrl(downloadUrl.toString());
+                                            Task<Uri> uriTask1 = uploadTaskImage.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
+                                                @Override
+                                                public Task<Uri> then(Task<UploadTask.TaskSnapshot> task) throws Exception {
+                                                    if(!task.isSuccessful())
+                                                    {
+                                                        throw task.getException();
+                                                    }
 
-                                            video.setName(videoName);
-                                            video.setVideourl(downloadUrl.toString());
-                                            video.setSearch(search);
-                                            String i = databaseReference.push().getKey();
-                                            databaseReference.child(i).setValue(video);
+                                                    return referenceImage.getDownloadUrl();
+                                                }
+                                            })
+                                                    .addOnCompleteListener(new OnCompleteListener<Uri>() {
+                                                        @Override
+                                                        public void onComplete(@NonNull Task<Uri> task) {
+                                                            if(task.isSuccessful())
+                                                            {
+                                                                Uri downloadUrl = task.getResult();
+                                                                video.setName(videoName);
+                                                                video.setCalo(calo);
+                                                                video.setSearch(search);
+                                                                video.setImageUrl(downloadUrl.toString());
+                                                                String i = databaseReference.push().getKey();
+                                                                databaseReference.child(i).setValue(video);
+                                                                progressBar.setVisibility(View.INVISIBLE);
+                                                                Toast.makeText(AddActivity.this,"Data saved", Toast.LENGTH_SHORT).show();
+                                                            }
+                                                        }
+                                                    });
+
                                         }else
                                         {
                                             Toast.makeText(AddActivity.this,"Failed", Toast.LENGTH_SHORT).show();
---
+++ app/src/main/java/com/example/appgym/model/Video.java
@@ -0,0 +1,54 @@
+package com.example.appgym.model;
+
+public class Video {
+    private String name;
+    private String videoUrl;
+    private String imageUrl;
+    private String search;
+    private String calo;
+
+    public Video()
+    {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getVideoUrl() {
+        return videoUrl;
+    }
+
+    public void setVideoUrl(String videoUrl) {
+        this.videoUrl = videoUrl;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getSearch() {
+        return search;
+    }
+
+    public void setSearch(String search) {
+        this.search = search;
+    }
+
+    public String getCalo() {
+        return calo;
+    }
+
+    public void setCalo(String calo) {
+        this.calo = calo;
+    }
+}
